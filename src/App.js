09e0a7aab037fec53bfbfc71e8ea0a997e14f65f
Dev <devmistry932@gmail.com>
2023-09-15 17:52:39 +0530

    ups

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 import MainRouter from './components/Routers/Router';
+// import axios from 'axios';
+
+// axios.defaults.baseURL = 'http://localhost:7000/';
+
 function App() {
   return (
     <React.Fragment>
