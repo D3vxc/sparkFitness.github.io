91cad7bf36a1682ef2636b6170576d3a3c9b0278
Dev <devmistry932@gmail.com>
2024-02-05 12:43:50 +0530

    svg, login and register style changes and css file

--- src/components/Credentials/Login.jsx
+++ src/components/Credentials/Login.jsx
@@ -1,18 +1,11 @@
-import * as React from "react";
+import React from "react";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
+import "./YourStyleSheet.css"; // Replace with the actual path to your CSS file
+import { Box, Typography, Grid } from "@mui/material";
 
 function Login() {
   const navigate = useNavigate();
@@ -87,132 +80,81 @@ function Login() {
   return (
     <React.Fragment>
       <ToastContainer />
-      <Container component='main' maxWidth='xs'>
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component='h1' variant='h5'>
-            Sign in
-          </Typography>
-          <Box
-            component='form'
-            onSubmit={handleSubmit(onSubmit)}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-            {/* Wrap TextField with Controller */}
-            <Controller
-              name='email'
-              control={control}
-              defaultValue=''
-              rules={{
-                required: "Email is required",
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                  message: "Invalid email address",
-                },
-              }}
-              render={({ field }) => (
-                <TextField
-                  {...field}
-                  margin='normal'
-                  required
-                  fullWidth
-                  id='email'
-                  label='Email Address'
-                  name='email'
-                  autoComplete='email'
-                  autoFocus
-                />
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+          background: "#212121",
+        }}
+      >
+        <div className='form-container'>
+          <form className='form' onSubmit={handleSubmit(onSubmit)} noValidate>
+            <div className='form-group'>
+              <label htmlFor='email'>Email</label>
+              <Controller
+                name='email'
+                control={control}
+                defaultValue=''
+                rules={{
+                  required: "Email is required",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "Invalid email address",
+                  },
+                }}
+                render={({ field }) => (
+                  <input {...field} required type='text' id='email' />
+                )}
+              />
+              {errors.email && (
+                <p className='error-message'>{errors.email.message}</p>
               )}
-            />
-            {errors.email && (
-              <Typography variant='caption' color='error'>
-                {errors.email.message}
-              </Typography>
-            )}
-            {/* Wrap TextField with Controller for Password */}
-            <Controller
-              name='password'
-              control={control}
-              defaultValue=''
-              rules={{
-                required: "Password is required",
-                minLength: {
-                  value: 8,
-                  message: "Password must be at least 8 characters",
-                },
-              }}
-              render={({ field }) => (
-                <TextField
-                  {...field}
-                  margin='normal'
-                  required
-                  fullWidth
-                  name='password'
-                  label='Password'
-                  type='password'
-                  id='password'
-                  autoComplete='current-password'
-                />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='password'>Password</label>
+              <Controller
+                name='password'
+                control={control}
+                defaultValue=''
+                rules={{
+                  required: "Password is required",
+                  minLength: {
+                    value: 8,
+                    message: "Password must be at least 8 characters",
+                  },
+                }}
+                render={({ field }) => (
+                  <input {...field} required type='password' id='password' />
+                )}
+              />
+              {errors.password && (
+                <p className='error-message'>{errors.password.message}</p>
               )}
-            />
-
-            {errors.password && (
-              <Typography variant='caption' color='error'>
-                {errors.password.message}
-              </Typography>
-            )}
-            {/* <FormControlLabel
-              control={<Checkbox value='remember' color='primary' />}
-              label='Remember me'
-            /> */}
-            <Button
+            </div>
+            <button
               type='submit'
-              fullWidth
-              variant='contained'
-              sx={{ mt: 3, mb: 2 }}
+              className='form-submit-btn'
               disabled={isLoading}
             >
               {isLoading ? "Logging in..." : "Sign In"}
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Typography
-                  variant='body2'
-                  onClick={() => {
-                    navigate("/forgot-password");
-                  }}
-                  sx={{
-                    cursor: "pointer",
-                  }}
-                >
-                  Forgot password?
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography
-                  variant='body2'
-                  onClick={() => {
-                    navigate("/register");
-                  }}
-                >
-                  {"Don't have an account? Sign Up"}
-                </Typography>
-              </Grid>
+            </button>
+          </form>
+          <Grid container justifyContent='flex-end'>
+            <Grid item>
+              <Typography
+                variant='body2'
+                onClick={() => {
+                  navigate("/register");
+                }}
+              >
+                Already have an account? Sign in
+              </Typography>
             </Grid>
-          </Box>
-        </Box>
-      </Container>
+          </Grid>
+        </div>
+      </Box>
     </React.Fragment>
   );
 }
--- src/components/Credentials/Register.jsx
+++ src/components/Credentials/Register.jsx
@@ -8,7 +8,6 @@ import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
@@ -18,20 +17,20 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "./YourStyleSheet.css"; // Replace with the actual path to your CSS file
 
 function Register() {
   const navigate = useNavigate();
   const [isVerified, setIsVerified] = useState(false);
   const {
     handleSubmit,
-    control, // Add control from react-hook-form
+    control,
     formState: { errors },
   } = useForm({
     mode: "onBlur",
     defaultValues: {
       email: "",
       phone: "",
-      username: "",
       password: "",
     },
   });
@@ -92,194 +91,157 @@ function Register() {
 
   return (
     <ThemeProvider theme={createTheme()}>
-      <Container component='main' maxWidth='xs'>
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component='h1' variant='h5'>
-            Sign up
-          </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+          background: "#212121", // Dark background color
+        }}
+      >
+        <Container component='main' maxWidth='xs'>
+          <CssBaseline />
           <Box
-            component='form'
-            noValidate
-            onSubmit={handleSubmit(onSubmit)}
-            sx={{ mt: 3 }}
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+            className='form-container' // Add the class for styling
           >
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                {/* Wrap TextField with Controller */}
-                <Controller
-                  name='email'
-                  control={control}
-                  defaultValue=''
-                  rules={{
-                    required: "Email is required",
-                    pattern: {
-                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                      message: "Invalid email address",
-                    },
-                  }}
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      required
-                      fullWidth
-                      id='email'
-                      label='Email Address'
-                      name='email'
-                      autoComplete='email'
-                    />
-                  )}
-                />
-                {errors.email && (
-                  <Typography variant='caption' color='error'>
-                    {errors.email.message}
-                  </Typography>
-                )}
-              </Grid>
-              <Grid item xs={12}>
-                {/* Wrap TextField with Controller for Phone Number */}
-                <Controller
-                  name='phone'
-                  control={control}
-                  defaultValue=''
-                  rules={{
-                    required: "Phone number is required",
-                    pattern: {
-                      value: /^[0-9]{10}$/i,
-                      message: "Invalid phone number",
-                    },
-                  }}
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      required
-                      fullWidth
-                      id='phone'
-                      label='Phone Number'
-                      name='phone'
-                      autoComplete='tel'
-                    />
-                  )}
-                />
-                {errors.phone && (
-                  <Typography variant='caption' color='error'>
-                    {errors.phone.message}
-                  </Typography>
-                )}
-              </Grid>
-              <Grid item xs={12}>
-                {/* Wrap TextField with Controller for Username */}
-                <Controller
-                  name='username'
-                  control={control}
-                  defaultValue=''
-                  rules={{
-                    required: "Username is required",
-                    minLength: {
-                      value: 6,
-                      message: "Username must be at least 6 characters",
-                    },
-                  }}
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      required
-                      fullWidth
-                      id='username'
-                      label='Username'
-                      name='username'
-                      autoComplete='username'
-                    />
-                  )}
-                />
-                {errors.username && (
-                  <Typography variant='caption' color='error'>
-                    {errors.username.message}
-                  </Typography>
-                )}
-              </Grid>
-              <Grid item xs={12}>
-                {/* Wrap TextField with Controller for Password */}
-                <Controller
-                  name='password'
-                  control={control}
-                  defaultValue=''
-                  rules={{
-                    required: "Password is required",
-                    minLength: {
-                      value: 8,
-                      message: "Password must be at least 8 characters",
-                    },
-                  }}
-                  render={({ field }) => (
-                    <TextField
-                      {...field}
-                      required
-                      fullWidth
-                      name='password'
-                      label='Password'
-                      type='password'
-                      id='password'
-                      autoComplete='new-password'
-                    />
-                  )}
-                />
-                {errors.password && (
-                  <Typography variant='caption' color='error'>
-                    {errors.password.message}
-                  </Typography>
-                )}
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      value={isVerified}
-                      onChange={handleCheckboxChange}
-                      color='primary'
-                    />
-                  }
-                  label='I am verified'
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type='submit'
-              fullWidth
-              variant='contained'
-              sx={{ mt: 3, mb: 2 }}
-              // onClick={() => {
-              //   console.log("Sign up");
-              //   navigate("/home");
-              // }}
+            <Box
+              component='form'
+              noValidate
+              onSubmit={handleSubmit(onSubmit)}
+              sx={{ mt: 3 }}
+              className='form' // Add the class for styling
             >
-              Sign Up
-            </Button>
-            <Grid container justifyContent='flex-end'>
-              <Grid item>
-                <Typography
-                  variant='body2'
-                  onClick={() => {
-                    navigate("/");
-                  }}
-                >
-                  Already have an account? Sign in
-                </Typography>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  {/* Wrap TextField with Controller for Email */}
+                  <Controller
+                    name='email'
+                    control={control}
+                    defaultValue=''
+                    rules={{
+                      required: "Email is required",
+                      pattern: {
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                        message: "Invalid email address",
+                      },
+                    }}
+                    render={({ field }) => (
+                      <TextField
+                        {...field}
+                        required
+                        fullWidth
+                        id='email'
+                        label='Email Address'
+                        name='email'
+                        autoComplete='email'
+                        className='form-group' // Add the class for styling
+                      />
+                    )}
+                  />
+                  {errors.email && (
+                    <Typography variant='caption' color='error'>
+                      {errors.email.message}
+                    </Typography>
+                  )}
+                </Grid>
+                <Grid item xs={12}>
+                  {/* Wrap TextField with Controller for Phone Number */}
+                  <Controller
+                    name='phone'
+                    control={control}
+                    defaultValue=''
+                    rules={{
+                      required: "Phone number is required",
+                      pattern: {
+                        value: /^[0-9]{10}$/i,
+                        message: "Invalid phone number",
+                      },
+                    }}
+                    render={({ field }) => (
+                      <TextField
+                        {...field}
+                        required
+                        fullWidth
+                        id='phone'
+                        label='Phone Number'
+                        name='phone'
+                        autoComplete='tel'
+                        className='form-group' // Add the class for styling
+                      />
+                    )}
+                  />
+                  {errors.phone && (
+                    <Typography variant='caption' color='error'>
+                      {errors.phone.message}
+                    </Typography>
+                  )}
+                </Grid>
+                <Grid item xs={12}>
+                  {/* Wrap TextField with Controller for Password */}
+                  <Controller
+                    name='password'
+                    control={control}
+                    defaultValue=''
+                    rules={{
+                      required: "Password is required",
+                      minLength: {
+                        value: 8,
+                        message: "Password must be at least 8 characters",
+                      },
+                    }}
+                    render={({ field }) => (
+                      <TextField
+                        {...field}
+                        required
+                        fullWidth
+                        name='password'
+                        label='Password'
+                        type='password'
+                        id='password'
+                        autoComplete='new-password'
+                        className='form-group' // Add the class for styling
+                      />
+                    )}
+                  />
+                  {errors.password && (
+                    <Typography variant='caption' color='error'>
+                      {errors.password.message}
+                    </Typography>
+                  )}
+                </Grid>
               </Grid>
-            </Grid>
+              <Button
+                type='submit'
+                fullWidth
+                variant='contained'
+                sx={{ mt: 3, mb: 2 }}
+                className='form-submit-btn' // Add the class for styling
+              >
+                Sign Up
+              </Button>
+              <Grid container justifyContent='flex-end'>
+                <Grid item>
+                  <Typography
+                    variant='body2'
+                    onClick={() => {
+                      navigate("/login");
+                    }}
+                  >
+                    Already have an account? Sign in
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Box>
           </Box>
-        </Box>
-      </Container>
+        </Container>
+      </Box>
       <ToastContainer />
     </ThemeProvider>
   );
