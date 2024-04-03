f8f0a18be30c16e3988ad43abaec68c5c38149c2
Dev <devmistry932@gmail.com>
2024-04-03 12:31:26 +0530

    Updates

--- src/components/Pages/Cart.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Cart() {
-  return <React.Fragment>cart page</React.Fragment>;
-}
-
-export default Cart;
--- src/utils/token.js
+++
@@ -1,9 +0,0 @@
-import Cookies from "js-cookie";
-
-export const getToken = () => {
-  const userToken = Cookies.get("token");
-  if (!userToken) {
-    window.location.href = "/login";
-  }
-  return userToken;
-};
